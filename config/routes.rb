d1a3d07e2de37ebafad8fdcbe6226457b40c2171
iwannatto <iwannatto@gmail.com>
2018-08-30 05:57:05 +0000

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :pdfs
+  resources :pdfs, format: false
+  root to: 'pdfs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
