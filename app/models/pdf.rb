1b6756d65b2fc630ff40ceb24ceebceb919c334e
iwannatto <iwannatto@gmail.com>
2018-09-12 02:08:07 +0000

    model test test

--- app/models/pdf.rb
+++ app/models/pdf.rb
@@ -1,4 +1,6 @@
 class Pdf < ApplicationRecord
   has_one_attached :pdf
   has_many_attached :jpegs
+  
+  validates :name, presence: true
 end
--- test/models/pdf_test.rb
+++ test/models/pdf_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PdfTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save pdf without name" do
+    pdf = Pdf.new
+    assert_not pdf.save, "saved"
+  end
 end
